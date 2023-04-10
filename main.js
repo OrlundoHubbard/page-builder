627cd072502133e3a7e078502f2e2066dd6e8da2
Orlundo Hubbard <weareprojectcode@gmail.com>
2023-04-09 23:36:44 -0500

    add js and stuff

---
+++ main.js
@@ -0,0 +1,11 @@
+const canvas = document.getElementById('canvas');
+
+function addText() {
+    const text = document.createElement('div');
+    text.innerText = 'Enter Text here';
+    canvas.appendChild(text);
+}
+
+function addImage() {
+    
+}
